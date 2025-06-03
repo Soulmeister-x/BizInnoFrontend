8d199bce9c3c49da53c23e419acd467e206dbf2a
Soulmeister-x <nickoeh@yahoo.de>
2025-06-03 22:00:52 +0200

    [ADD] basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,17 @@
 	let { children } = $props();
 </script>
 
+<nav class="flex h-10 items-center">
+	<div class="bg-blue-500 h-full w-1/4 flex items-center justify-center">
+		<h1>COMPANY</h1>
+	</div>
+	<div class="bg-amber-600 h-full flex grow justify-evenly items-center">
+		<a href="/">home</a>
+		<a href="/search">Search</a>
+		<a href="/inbox">Inbox</a>
+		<a href="/user">Profile</a>
+		<a href="/tenders">Tenders</a>
+	</div>
+</nav>
+
 {@render children()}
