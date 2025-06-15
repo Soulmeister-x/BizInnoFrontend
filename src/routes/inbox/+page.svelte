c4f3fb4336d9b7152b7864eb86fb528a95682192
Soulmeister-x <nickoeh@yahoo.de>
2025-06-15 16:27:57 +0200

    [ADD] toggle behavior

--- src/routes/inbox/+page.svelte
+++ src/routes/inbox/+page.svelte
@@ -1,21 +1,40 @@
 <script>
-	let items = [
+	let items = $state([
 		{
 			title: 'Ausschreibung 1',
-			summary: 'Lorem Ipsum dolor met...'
+			summary: 'Lorem Ipsum dolor met...',
+			feedback: 'Score: 8.2 --> You should consider applying.',
+			showSummary: false,
+			showFeedback: false
 		},
 		{
 			title: 'Ausschreibung 2',
-			summary: 'Lorem Ipsum dolor met...'
+			summary: 'Lorem Ipsum dolor met...',
+			feedback: 'Score: 9.4 --> You should apply.',
+			showSummary: false,
+			showFeedback: false
 		},
 		{
 			title: 'Ausschreibung 3',
-			summary: 'Lorem Ipsum dolor met...'
+			summary: 'Lorem Ipsum dolor met...',
+			feedback: 'Score: 5.4 --> Application not suggested.',
+			showSummary: false,
+			showFeedback: false
 		}
-	];
+	]);
 
 	let start_date = $state('08/05/25');
 	let end_date = $state();
+
+	function toggleVisibility(item, type) {
+		if (type === 'summary') {
+			item.showSummary = !item.showSummary;
+			if (item.showFeedback) item.showFeedback = false;
+		} else if (type === 'feedback') {
+			item.showFeedback = !item.showFeedback;
+			if (item.showSummary) item.showSummary = false;
+		}
+	}
 </script>
 
 <link rel="stylesheet" href="icons.css" />
@@ -48,17 +67,31 @@
 				<div class="bg-white w-full mt-8 p-6 rounded-xl">
 					<div class="flex pt-4 pb-4">
 						<h2 class="grow self-center text-2xl">{item.title}</h2>
-						<div class="flex bg-red-950 text-white ml-6 p-4 rounded-sm place-items-center">
+						<button
+							onclick={() => {
+								toggleVisibility(item, 'summary');
+							}}
+							class="flex bg-red-950 text-white ml-6 p-4 rounded-sm place-items-center"
+						>
 							<p>Summary</p>
 							<span class="material-symbols--clipboard p-4 ml-2"></span>
-						</div>
-						<div class="flex bg-green-800 text-white ml-6 p-4 rounded-sm place-items-center">
+						</button>
+						<button
+							onclick={() => {
+								toggleVisibility(item, 'feedback');
+							}}
+							class="flex bg-green-800 text-white ml-6 p-4 rounded-sm place-items-center"
+						>
 							<p>Feedback</p>
 							<span class="material-symbols--pencil p-4 ml-2"></span>
-						</div>
+						</button>
 					</div>
-					<div class="border-1 border-gray-400 p-6 rounded-xl">
-						<p>{item.summary}</p>
+					<div class="border-1 border-gray-400 rounded-xl">
+						{#if item.showSummary}
+							<p class="m-6">{item.summary}</p>
+						{:else if item.showFeedback}
+							<p class="m-6">{item.feedback}</p>
+						{/if}
 					</div>
 				</div>
 			{/each}
