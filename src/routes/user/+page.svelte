ded94211bb66acc28635c34d105e28bd6c3689c5
Soulmeister-x <nickoeh@yahoo.de>
2025-06-25 02:48:31 +0200

    [ADD] profile form

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -5,6 +5,7 @@
 	let userProfile = $state();
 	let start_date = $state('08/05/25');
 	let end_date = $state();
+	let edit_mode_enabled = $state(false);
 
 	onMount(async function () {
 		const response = await fetch(endpoint);
@@ -15,34 +16,74 @@
 
 <svelte:head>
 	<title>Profil</title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 </svelte:head>
 
 <main class="pt-8">
 	<div class="container mx-auto p-4 border-2 rounded-lg shadow-md">
+		<div class="relative">
+			<button
+				class="absolute right-0 cursor-pointer"
+				onclick={() => (edit_mode_enabled = !edit_mode_enabled)}
+			>
+				{#if edit_mode_enabled}
+					<i class="material-icons">close</i>
+				{:else}
+					<i class="material-icons">edit_square</i>
+				{/if}
+			</button>
+		</div>
 		<h1 class="text-2xl font-bold text-center mb-4">User Profile</h1>
-
-		{#if userProfile}
-			<div class="mb-4">
-				<p class="text-gray-700"><strong>Company Name:</strong> {userProfile.name}</p>
+		<div id="profile-table" class="columns-2">
+			<div>
+				<div><label for="name"><strong>Company Name:</strong></label></div>
+				<div><label for="branche"><strong>Branche:</strong></label></div>
+				<div><label for="specialization"><strong>Specialization:</strong></label></div>
+				<div><label for="city"><strong>City:</strong></label></div>
+				<div><label for="zipcode"><strong>Zip Code:</strong></label></div>
+				<div><label for="search_radius"><strong>Search Radius:</strong></label></div>
 			</div>
-
-			<div class="mb-4">
-				<p class="text-gray-700"><strong>Type:</strong> {userProfile.type_name}</p>
+			<div>
+				{#if edit_mode_enabled}
+					<form method="post">
+						<div><input type="text" value={userProfile.name} name="name" id="name" /></div>
+						<div>
+							<input type="text" value={userProfile.type_name} name="branche" id="branche" />
+						</div>
+						<div>
+							<input
+								type="text"
+								value={userProfile.specialization}
+								name="specialization"
+								id="specialization"
+							/>
+						</div>
+						<div><input type="text" value={userProfile.region.city} name="city" id="city" /></div>
+						<div>
+							<input type="text" value={userProfile.region.zipcode} name="zipcode" id="zipcode" />
+						</div>
+						<div>
+							<input
+								type="text"
+								value={userProfile.region.radius}
+								name="search_radius"
+								id="search_radius"
+							/>
+						</div>
+					</form>
+				{:else if userProfile}
+					<div><p>{userProfile.name}</p></div>
+					<div><p>{userProfile.type_name}</p></div>
+					<div><p>{userProfile.specialization}</p></div>
+					<div><p>{userProfile.region.city}</p></div>
+					<div><p>{userProfile.region.zipcode}</p></div>
+					<div><p>{userProfile.region.radius} km</p></div>
+				{/if}
 			</div>
-
-			<div class="mb-4">
-				<p class="text-gray-700"><strong>Specialization:</strong> {userProfile.specialization}</p>
-			</div>
-
-			<div class="mb-4">
-				<p class="text-gray-700">
-					<strong>Location:</strong>
-					{userProfile.region.city}, {userProfile.region.zipcode}
-				</p>
-			</div>
-
-			<div class="mb-4">
-				<p class="text-gray-700"><strong>Search Radius:</strong> {userProfile.region.radius} km</p>
+		</div>
+		{#if edit_mode_enabled}
+			<div class="relative mt-8">
+				<button class="absolute bottom-0 right-0 cursor-pointer" type="submit">Send</button>
 			</div>
 		{/if}
 	</div>
@@ -52,5 +93,11 @@
 	.container {
 		max-width: 600px;
 		margin: 0 auto;
+	}
+	#profile-table > div {
+		margin-bottom: 4px;
+	}
+	input {
+		width: 100%;
 	}
 </style>
