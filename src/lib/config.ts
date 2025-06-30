72314ee0feff0999dbef57834264ca0248dd55bc
Soulmeister-x <nickoeh@yahoo.de>
2025-06-30 14:02:34 +0200

    [UPDATE] api base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,2 @@
-export const BASE_API = 'https://livavivo.de/api/v1';
+//export const BASE_API = 'https://livavivo.de/api/v1';
+export const BASE_API = 'https://buck-fine-randomly.ngrok-free.app/api/v1'
